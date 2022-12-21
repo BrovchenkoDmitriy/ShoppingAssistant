e48b38eb7b3270da91218bdf9ed1606134edaaa9
BrovchenkoDmitriy <99586855+BrovchenkoDmitriy@users.noreply.github.com>
2022-12-21 20:43:03 +0900

    add Jetpack Navigation. Refactor binding init in fragments (it was incorrect)

--- app/src/main/java/com/example/shoppingassistant/presentation/MainMenuFragment.kt
+++ app/src/main/java/com/example/shoppingassistant/presentation/MainMenuFragment.kt
@@ -5,37 +5,43 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.shoppingassistant.R
 import com.example.shoppingassistant.databinding.FragmentMainMenuBinding
+
 
 class MainMenuFragment : Fragment() {
 
     private var _binding: FragmentMainMenuBinding? = null
-    private val binding: FragmentMainMenuBinding =
-        _binding ?: throw RuntimeException("FragmentMainMenuBinding is null")
+    private val binding: FragmentMainMenuBinding
+        get() {
+            return _binding ?: throw RuntimeException("FragmentMainMenuBinding is null")
+        }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentMainMenuBinding.inflate(layoutInflater, container, false)
+        _binding = FragmentMainMenuBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
         return binding.root
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        _binding=null
+        _binding = null
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonShoppingList.setOnClickListener {
-            TODO()
+            findNavController().navigate(R.id.action_mainMenuFragment_to_shoppingListFragment)
         }
         binding.buttonNotes.setOnClickListener {
-            TODO()
+            findNavController().navigate(R.id.action_mainMenuFragment_to_noteListFragment)
         }
     }
 
--- app/src/main/java/com/example/shoppingassistant/presentation/NoteListFragment.kt
+++ app/src/main/java/com/example/shoppingassistant/presentation/NoteListFragment.kt
@@ -1,33 +1,30 @@
 package com.example.shoppingassistant.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.shoppingassistant.R
+import androidx.fragment.app.Fragment
 import com.example.shoppingassistant.databinding.FragmentNoteListBinding
-import com.example.shoppingassistant.databinding.FragmentShoppingListBinding
-import java.lang.RuntimeException
 
 class NoteListFragment : Fragment() {
 
     private var _binding: FragmentNoteListBinding? = null
-    private val binding: FragmentNoteListBinding =
-        _binding ?: throw RuntimeException("FragmentShoppingListBinding is null")
+    private val binding: FragmentNoteListBinding
+        get() = _binding ?: throw RuntimeException("FragmentShoppingListBinding is null")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        _binding = FragmentNoteListBinding.inflate(layoutInflater,container,false)
+        _binding = FragmentNoteListBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        _binding=null
+        _binding = null
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
